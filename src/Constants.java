e0ac4086412f97843b7362e2942c0d8bfcaaea86
lau1406 <lkeijzer@live.nl>
2017-04-12 16:45:30 +0200

    add way to set the base and amount of characters

---
+++ src/Constants.java
@@ -0,0 +1,12 @@
+/**
+ * Created by L. Keijzer on 2017-04-10.
+ * Class to store constants
+ */
+public class Constants {
+    public static final String ARG_BASE = "--set-base";
+    public static final String ARG_AMOUNT_CHARACTERS = "--set-characters";
+
+
+    private Constants() {
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,212 +1,27 @@
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by L. Keijzer on 2017-04-08.
  */
 public class Main {
-    /**
-     * base 3 zero = .
-     * base 3 one = '
-     * base 3 two = :
-     */
 
-    // Map from alphabet to base
-    private final Map<Character, String> mMapAB = new HashMap<Character, String>() {
-        {
-            put('a', "....");
-            put('b', "...'");
-            put('c', "...:");
-            put('d', "..'.");
-            put('e', "..''");
-            put('f', "..':");
-            put('g', "..:.");
-            put('h', "..:'");
-            put('i', "..::");
-            put('j', ".'..");
-            put('k', ".'.'");
-            put('l', ".'.:");
-            put('m', ".''.");
-            put('n', ".'''");
-            put('o', ".'':");
-            put('p', ".':.");
-            put('q', ".':'");
-            put('r', ".'::");
-            put('s', ".:..");
-            put('t', ".:.'");
-            put('u', ".:.:");
-            put('v', ".:'.");
-            put('w', ".:''");
-            put('x', ".:':");
-            put('y', ".::.");
-            put('z', ".::'");
-            put(' ', ".:::");
-
-            put('A', "'...");
-            put('B', "'..'");
-            put('C', "'..:");
-            put('D', "'.'.");
-            put('E', "'.''");
-            put('F', "'.':");
-            put('G', "'.:.");
-            put('H', "'.:'");
-            put('I', "'.::");
-            put('J', "''..");
-            put('K', "''.'");
-            put('L', "''.:");
-            put('M', "'''.");
-            put('N', "''''");
-            put('O', "''':");
-            put('P', "'':.");
-            put('Q', "'':'");
-            put('R', "''::");
-            put('S', "':..");
-            put('T', "':.'");
-            put('U', "':.:");
-            put('V', "':'.");
-            put('W', "':''");
-            put('X', "':':");
-            put('Y', "'::.");
-            put('Z', "'::'");
-            put('_', "':::");
-
-            put('0', ":...");
-            put('1', ":..'");
-            put('2', ":..:");
-            put('3', ":.'.");
-            put('4', ":.''");
-            put('5', ":.':");
-            put('6', ":.:.");
-            put('7', ":.:'");
-            put('8', ":.::");
-            put('9', ":'..");
-            put(',', ":'.'");
-            put('.', ":'.:");
-            put('?', ":''.");
-            put('!', ":'''");
-            put('@', ":'':");
-            put(':', ":':.");
-            put('|', ":':'");
-            put('\\', ":'::");
-            put('\'', "::..");
-            put(':', "::.'");
-            put(';', "::.:");
-            put('"', "::'.");
-            put('/', "::''");
-            put('-', "::':");
-            put('=', ":::.");
-            put('+', ":::'");
-            put('*', "::::");
-        }
-    };
-
-    // Map from base to alphabet
-    private final Map<String, Character> mMapBA = new HashMap<String, Character>() {
-        {
-            put("....", 'a');
-            put("...'", 'b');
-            put("...:", 'c');
-            put("..'.", 'd');
-            put("..''", 'e');
-            put("..':", 'f');
-            put("..:.", 'g');
-            put("..:'", 'h');
-            put("..::", 'i');
-            put(".'..", 'j');
-            put(".'.'", 'k');
-            put(".'.:", 'l');
-            put(".''.", 'm');
-            put(".'''", 'n');
-            put(".'':", 'o');
-            put(".':.", 'p');
-            put(".':'", 'q');
-            put(".'::", 'r');
-            put(".:..", 's');
-            put(".:.'", 't');
-            put(".:.:", 'u');
-            put(".:'.", 'v');
-            put(".:''", 'w');
-            put(".:':", 'x');
-            put(".::.", 'y');
-            put(".::'", 'z');
-            put(".:::", ' ');
-
-            put("'...", 'A');
-            put("'..'", 'B');
-            put("'..:", 'C');
-            put("'.'.", 'D');
-            put("'.''", 'E');
-            put("'.':", 'F');
-            put("'.:.", 'G');
-            put("'.:'", 'H');
-            put("'.::", 'I');
-            put("''..", 'J');
-            put("''.'", 'K');
-            put("''.:", 'L');
-            put("'''.", 'M');
-            put("''''", 'N');
-            put("''':", 'O');
-            put("'':.", 'P');
-            put("'':'", 'Q');
-            put("''::", 'R');
-            put("':..", 'S');
-            put("':.'", 'T');
-            put("':.:", 'U');
-            put("':'.", 'V');
-            put("':''", 'W');
-            put("':':", 'X');
-            put("'::.", 'Y');
-            put("'::'", 'Z');
-            put("':::", '_');
-
-            put(":...", '0');
-            put(":..'", '1');
-            put(":..:", '2');
-            put(":.'.", '3');
-            put(":.''", '4');
-            put(":.':", '5');
-            put(":.:.", '6');
-            put(":.:'", '7');
-            put(":.::", '8');
-            put(":'..", '9');
-            put(":'.'", ',');
-            put(":'.:", '.');
-            put(":''.", '?');
-            put(":'''", '!');
-            put(":'':", '@');
-            put(":':.", ':');
-            put(":':'", '|');
-            put(":'::", '\\');
-            put("::..", '\'');
-            put("::.'", ':');
-            put("::.:", ';');
-            put("::'.", '"');
-            put("::''", '/');
-            put("::':", '-');
-            put(":::.", '=');
-            put(":::'", '+');
-            put("::::", '*');
-        }
-    };
-
-    private final char[] VALID_CHARS_ALPHABET = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    private final Character[] VALID_CHARS_ALPHABET = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
             'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', '0',
-            '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.', '?', '!', '@', ':', '|', '\\', '\'', ':', ';',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.', '?', '!', '|', '(', ')', '\\', '\'', ':', ';',
             '"', '/', '-', '=', '+', '*',};
-    private final char[] VALID_CHARS_BASE_3 = {'.', '\'', ':'};
+    private final Character[] VALID_CHARS_BASE = {'.', '\'', ':', '*', '/', '\\', '-', '?', '+', '_'};   // 10 Characters
+    // for max base of 10
 
     private boolean[] isValid;
     private boolean isAlphabet = true;
     private boolean isBase3 = true;
 
-    private final int AMOUNT_CHARACTERS = 4;
-    private final int BASE = 3;
+    private static int mAmountCharacters = 4;
+    private static int mBase = 3;
 
     private Scanner mScanner;
     private StringBuilder mBuilder = new StringBuilder();
@@ -215,6 +30,12 @@ public class Main {
 
     public Main() {
         mScanner = new Scanner(System.in);
+
+        Map[] maps = genMaps();
+        // Map from alphabet to base
+        final Map<Character, String> mMapAB = maps[0];
+        // Map from base to alphabet
+        final Map<String, Character> mMapBA = maps[1];
 
         while (true) {
             System.out.println("Input please: ");
@@ -237,8 +58,8 @@ public class Main {
 
             // Check if is  a valid base 3
             isValid = new boolean[Character.MAX_VALUE + 1];
-            for (char c : VALID_CHARS_BASE_3) {
-                isValid[c] = true;
+            for (int i = 0; i < mBase; i++) {
+                isValid[VALID_CHARS_BASE[i]] = true;
             }
             for (char c : inputChars) {
                 if (!isValid[c]) {
@@ -249,14 +70,14 @@ public class Main {
 
             if (isAlphabet || isBase3) {
                 mBuilder.replace(0, mBuilder.length(), "");
-                // VALID_CHARS_BASE_3 is subset of VALID_CHARS_ALPHABET
+                // VALID_CHARS_BASE is subset of VALID_CHARS_ALPHABET
                 if (isBase3) {
                     mSmallBuilder.replace(0, mSmallBuilder.length(), "");
                     int count = 0;
                     for (char c : inputChars) {
                         count++;
                         mSmallBuilder.append(c);
-                        if (count == AMOUNT_CHARACTERS) {
+                        if (count == mAmountCharacters) {
                             mBuilder.append(mMapBA.get(mSmallBuilder.toString()));
                             mSmallBuilder.replace(0, mSmallBuilder.length(), "");
                             count = 0;
@@ -267,19 +88,9 @@ public class Main {
                         mBuilder.append(mMapAB.get(c));
                     }
                     Random random = new Random();
-                    int amount = random.nextInt(AMOUNT_CHARACTERS);
+                    int amount = random.nextInt(mAmountCharacters);
                     for (int i = 0; i < amount; i++) {
-                        switch (random.nextInt(BASE)) {
-                            case 0:
-                                mBuilder.append(".");
-                                break;
-                            case 1:
-                                mBuilder.append("'");
-                                break;
-                            case 2:
-                                mBuilder.append(":");
-                                break;
-                        }
+                        mBuilder.append(VALID_CHARS_BASE[random.nextInt(mBase)]);
                     }
                 }
                 System.out.println("Output: " + mBuilder.toString());
@@ -295,7 +106,85 @@ public class Main {
         }
     }
 
+    /**
+     * Method that generates two Maps.
+     * {@code Map[0]} contains {@code mapAB}, a mapping from alphanumeric to base encoded.
+     * {@code Map[1]} contains {@code mapBA}, a mapping from base to alphanumeric encoded.
+     * @return {@code Map[]} containing two Maps that are the same but the direction inverted
+     */
+    private Map[] genMaps() {
+        Map<Character, String> mapAB = new HashMap<>();
+        Map<String, Character> mapBA = new HashMap<>();
+
+        int amountOfCharactersPossible = (int) Math.pow(mBase, mAmountCharacters);
+        for (int i = 0; i < Math.min(amountOfCharactersPossible, VALID_CHARS_ALPHABET.length); i++) {
+            Character character = VALID_CHARS_ALPHABET[i];
+            StringBuilder stringy = new StringBuilder();
+            int remaining = i;
+            for (int j = mAmountCharacters - 1; j >= 0; j--) {
+                int value = (int) Math.pow(mBase, j); // Get the values of the jth character
+                int amount = 0;
+                while (remaining - value >= 0) {
+                    remaining -= value;
+                    amount++;
+                }
+                stringy.append(VALID_CHARS_BASE[amount]);
+            }
+            mapAB.put(character, stringy.toString());
+            mapBA.put(stringy.toString(), character);
+            System.out.println("adding: " + character + " : " + stringy.toString());
+        }
+        return new Map[]{mapAB, mapBA};
+    }
+
+    private static void setBase(int base) {
+        if (base > 1 && base <= 10) {
+            mBase = base;
+            System.out.println("Set base to " + mBase);
+        } else {
+            System.out.println("Base of " + base + " not between 2 and 10.");
+        }
+    }
+
+    private static void setCharacters(int amount) {
+        if (amount > 1 && amount <= 10) {
+            mAmountCharacters = amount;
+            System.out.println("Set amount of characters to " + mAmountCharacters);
+        } else {
+            System.out.println("Amount of characters of " + amount + " not between 2 and 10.");
+        }
+    }
+
     public static void main(String[] args) {
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                switch (args[i]) {
+                    case Constants.ARG_BASE:
+                        if (args.length > i + 1) {
+                            try {
+                                setBase(Integer.parseInt(args[i + 1]));
+                            } catch (NumberFormatException e) {
+                                System.out.println("Arg was wrong, not setting the base.");
+                            }
+                        } else {
+                            System.out.println("Missing base variable");
+                        }
+                        break;
+                    case Constants.ARG_AMOUNT_CHARACTERS:
+                        if (args.length > i + 1) {
+                            try {
+                                setCharacters(Integer.parseInt(args[i + 1]));
+                            } catch (NumberFormatException e) {
+                                System.out.println("Arg was wrong, not setting the amount of characters.");
+                            }
+                        } else {
+                            System.out.println("Missing amount of characters variable");
+                        }
+                        break;
+                }
+            }
+        }
+
         new Main();
     }
 }
